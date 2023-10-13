44adbf584d3386c455fb44adb0487d76de290314
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-10-13 00:47:35 -0500

    feat: Added loading component

---
+++ src/components/core/PlaceholderTable.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { rows, cols } = defineProps({ rows: Number, cols: Number })
+</script>
+<template>
+    <tr v-for="row in rows" :key="row">
+        <td v-for="col in cols" :key="col">
+            <p class="placeholder-glow m-0">
+                <span class="placeholder w-100"></span>
+            </p>
+        </td>
+    </tr>
+</template>
--- src/components/shopping-lists/ShoppingListIndex.vue
+++ src/components/shopping-lists/ShoppingListIndex.vue
@@ -1,33 +1,36 @@
 <script setup lang="ts">
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import { useShoppingListService } from '@/services/shoppingListService'
 import { ShoppingListInterface } from '@/types'
-import ShoppingList from '@/components/shopping-lists/ShoppingList.vue'
 import ThreeDotsVerticalIcon from '@/components/icons/ThreeDotsVerticalIcon.vue'
-import Search from '../layout/Search.vue'
+import RefreshIcon from '@/components/icons/RefreshIcon.vue'
+import Search from '@/components/layout/Search.vue'
+import Loading from 'vue-loading-overlay'
+import ShoppingList from './ShoppingList.vue'
 
 const shoppingListService = useShoppingListService()
+const initialShoppingLists = () => ([] as ShoppingListInterface[])
+const shoppingLists = reactive(initialShoppingLists())
+const isLoading = ref(false)
 
-const initialState = () => ({
-    shoppingLists: [] as ShoppingListInterface[]
-})
+const get = async () => {
+    isLoading.value = true
+    shoppingListService.index().then((response: { data: any[], links: any, meta: any }) => {
+        shoppingLists.length = 0
+        response.data.forEach(item => {
+            const shoppingList = {
+                total: item.total,
+                isPaid: item.is_paid,
+                shop: {
+                    name: item.shop.name
+                }
+            } as ShoppingListInterface
+            shoppingLists.push(shoppingList)
+        })
+    }).finally(() => isLoading.value = false)
+}
 
-onMounted(async () => {
-    const response: { data: any[], links: any, meta: any } = await shoppingListService.index()
-    response.data.forEach(item => {
-        const shoppingList = {
-            total: item.total,
-            isPaid: item.is_paid,
-            shop: {
-                name: item.shop.name
-            }
-        } as ShoppingListInterface
-
-        state.shoppingLists.push(shoppingList)
-    })
-})
-
-const state = reactive(initialState())
+onMounted(async () => get())
 
 </script>
 <template>
@@ -39,12 +42,20 @@ const state = reactive(initialState())
                     <Search />
                 </div>
                 <div class="col col-auto">
-                    <button type="button" class="btn btn-sm btn-actions"><ThreeDotsVerticalIcon /></button>
+                    <button class="btn btn-actions btn-sm" @click="get">
+                        <RefreshIcon />
+                    </button>
+                </div>
+                <div class="col col-auto">
+                    <button type="button" class="btn btn-sm btn-actions">
+                        <ThreeDotsVerticalIcon />
+                    </button>
                 </div>
             </div>
         </div>
-        <div class="card-body">
-            <ShoppingList v-for="(shoppingList, index) in state.shoppingLists" :key="index" :shopping-list="shoppingList" />
+        <div class="vl-parent card-body d-flex gap-3">
+            <loading v-model:active="isLoading" :is-full-page="false" />
+            <ShoppingList v-for="(shoppingList, index) in shoppingLists" :shopping-list="shoppingList" :key="index"/>
         </div>
     </div>
 </template>
